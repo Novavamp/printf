ebe95c0ea2965d265054017eaacf8ba6d234c72a
Precious Nwosu <114708887+Novavamp@users.noreply.github.com>
2023-06-24 14:03:09 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,9 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
 
 int _putchar(char c);
+int _getchar(void);
+
 int _print_char(va_list args);
 int _print_string(va_list args);
 int _print_int(va_list args);
