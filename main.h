c2d14dd918ee57b63fba8d5ef6bf307e749da96c
Precious Nwosu <114708887+Novavamp@users.noreply.github.com>
2023-06-24 17:54:31 +0100

    Update main.h

--- main.h
+++ main.h
@@ -9,8 +9,7 @@ int _putchar(char c);
 int _getchar(void);
 
 int print_binary(va_list list);
-int binary_helper(int value);
-int _print_binary(unsigned int num);
+int binary_helper(unsigned int value);
 int print_number_base(int num, int base);
 int print_number(int num);
 int format_specifier(char c, va_list args);
